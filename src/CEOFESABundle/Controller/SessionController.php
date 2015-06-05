8bef410686d08cf9612f1335ddad211c031db27a
Mael ORTIE <mael-o@hotmail.fr>
2015-06-05 17:26:20 +0200

    début de formulaire de choix dans vue

--- src/CEOFESABundle/Controller/SessionController.php
+++ src/CEOFESABundle/Controller/SessionController.php
@@ -3,8 +3,11 @@
 namespace CEOFESABundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 
 /**
  * Session controller.
@@ -18,17 +21,54 @@ class SessionController extends Controller
     * 	path="/",
     * 	name="session_index"
     * )
+    * @Method({"GET","POST"})
     * @Template("::Session\index.html.twig")
     */
     public function indexAction(Request $request)
     {
-       	$em = $this->getDoctrine()->getManager();
+       	$form = $this->createChooseForm();
 		$id = $this->get('session')->get('structure');
 
-        $modules = $em->getRepository('CEOFESABundle:Modules')->findAll();
+		if ($request->isMethod('POST')) {
+            $form->bind($request);
+
+            // data is an array
+            $data = $form->getData();
+        }
 
         return array(
-            'modules' => $modules,
+            'choose_form'   => $form->createView(),
         );
+    }
+
+    /**
+     * Création d'un formulaire pour choisir les "paramètres" des sessions à afficher
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createChooseForm()
+    {
+        $id = $this->get('session')->get('structure');
+        $em = $this->getDoctrine()->getManager();
+
+        $data = array();
+    	$form = $this->createFormBuilder($data)
+        	->add('module','entity',array(
+                'class' => 'CEOFESABundle\Entity\Module',
+                'property' => 'modCode',
+                'multiple' => false
+            ))
+        	->add('type','entity',array(
+                'class' => 'CEOFESABundle\Entity\ModuleT',
+                'property' => 'mtyType',
+                'multiple' => false
+            ))
+        	->add('message', 'text')
+        	->add('voir','submit', array(
+                'attr' => array('class' => 'btn-primary')
+            ))
+        	->getForm();
+
+        return $form;
     }
 }
