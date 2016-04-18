480ad9206f0247be8dace9b8b343d570f362a491
Florent Rosso <flo_r14@bbox.fr>
2016-04-18 16:17:22 +0200

    ajout visualisation envoi/retour des conventions de sous-traitance

--- src/CEOFESABundle/Form/Type/RelationType.php
+++ src/CEOFESABundle/Form/Type/RelationType.php
@@ -30,6 +30,42 @@ class RelationType extends AbstractType
             ))
             ->add('relDateDebut', 'date', array('label' => 'Date de début de la sous-traitance'))
             ->add('relDateFin', 'date', array('label' => 'Date de fin de la sous-traitance'))
+            ->add('relEnvoiconvention', 'checkbox', array(
+                'label'    => 'Convention envoyée',
+                'required' => false
+            ))
+            ->add('relRetourconvention', 'checkbox', array(
+                'label'    => 'Convention revenue',
+                'required' => false,
+            ))
+            ->add('relEnvoiavenant', 'checkbox', array(
+                'label'    => 'Avenant envoyé',
+                'required' => false,
+            ))
+            ->add('relRetouravenant', 'checkbox', array(
+                'label'    => 'Avenant revenu',
+                'required' => false,
+            ))
+            ->add('relDateenvoiconvention', 'date', array(
+                'label'       => 'Date d\'envoi de la convention',
+                'placeholder' => "",
+                'required'    => false,
+            ))
+            ->add('relDateretourconvention', 'date', array(
+                'label'       => 'Date de retour de la convention',
+                'placeholder' => "",
+                'required'    => false,
+            ))
+            ->add('relDateenvoiavenant', 'date', array(
+                'label'       => 'Date d\'envoi de l\'avenant',
+                'placeholder' => "",
+                'required'    => false,
+            ))
+            ->add('relDateretouravenant', 'date', array(
+                'label'       => 'Date de retour de l\'avenant',
+                'placeholder' => "",
+                'required'    => false,
+            ))
             ->add('enregistrer', 'submit', array(
                 'attr' => array('class' => 'btn-primary')
             ))
