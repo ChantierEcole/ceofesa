921f78f17f1b1b41bfa4822a450b7e39cd0c3058
Eric GELOEN <geloen.eric@gmail.com>
2016-11-08 12:29:46 +0100

    [CE-1] Fix DAF creation (#17)

--- src/CEOFESABundle/Form/Type/ParcoursType.php
+++ src/CEOFESABundle/Form/Type/ParcoursType.php
@@ -2,116 +2,48 @@
 
 namespace CEOFESABundle\Form\Type;
 
+use CEOFESABundle\Form\Subscriber\ParcoursSubscriber;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use CEOFESABundle\Repository\StructureRepository;
-use CEOFESABundle\Repository\TiersRepository;
 
 class ParcoursType extends AbstractType
 {
-     public function __construct ($idStructure)
-    {
-        $this->idStructure = $idStructure;
-    }
-
     /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
+     * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('prcModule','entity',array(
-                'class' => 'CEOFESABundle\Entity\Module',
+            ->add('prcModule', 'entity', array(
+                'class'    => 'CEOFESABundle\Entity\Module',
                 'property' => 'modCode',
-                'label' => 'Module'
+                'label'    => 'Module',
             ))
-            ->add('prcNombreheure','number', array(
-                'label' => "Nb Heures",
+            ->add('prcNombreheure', 'number', array(
+                'label' => 'Nb Heures',
             ))
-            ->add('prcType','entity',array(
-                'class' => 'CEOFESABundle\Entity\ModuleT',
+            ->add('prcType', 'entity', array(
+                'class'    => 'CEOFESABundle\Entity\ModuleT',
                 'property' => 'mtyType',
-                'label' => 'Type'
+                'label'    => 'Type',
             ))
-        ;
-
-        $builder->addEventListener(
-            FormEvents::PRE_SET_DATA,
-            function(FormEvent $event) {
-
-                $form = $event->getForm();
-
-                $data = $event->getData();
-
-                if($data != null){
-                    $type = $data->getPrcType()->getMtyType();
-                } else {
-                    $type = "INTRA";
-                }
-
-                if($type == "INTRA") {
-                    $form->add('prcStructure','entity',array(
-                        'class' => 'CEOFESABundle\Entity\Structure',
-                        'property' => 'strNom',
-                        'label' => 'OF',
-                        'multiple' => false,
-                        'query_builder' => function(StructureRepository $repo){
-                            return $repo->getIntra();
-                        }
-                    ));
-                } elseif($type == "EXTERNE") {
-                    $id = $this->idStructure;
-                    $form->add('prcStructure','entity',array(
-                        'class' => 'CEOFESABundle\Entity\Structure',
-                        'property' => 'strNom',
-                        'label' => 'OF',
-                        'multiple' => false,
-                        'query_builder' => function(StructureRepository $repo) use ($id){
-                            return $repo->getSoustraitants($id);
-                        }
-                    ));
-                }
-            }
-        );
-
-        $builder->addEventListener(FormEvents::PRE_SUBMIT, array($this, 'onPreSubmit'));
-    }
-
-    public function onPreSubmit(FormEvent $event) {
-        $form = $event->getForm();
-        $data = $event->getData();
-
-        $ofId = $data['prcStructure'];
-        if($ofId != null){
-            $form->remove('prcStructure');
-            $form->add('prcStructure','entity',array(
-                'class' => 'CEOFESABundle\Entity\Structure',
-                'property' => 'strNom',
-                'label' => 'OF',
-                'multiple' => false,
-                'query_builder' => function(StructureRepository $repo) use($ofId) {
-                    return $repo->getSoustraitant($ofId);
-                }
-            ));
-        }
+            ->addEventSubscriber(new ParcoursSubscriber());
     }
 
     /**
-     * @param OptionsResolverInterface $resolver
+     * {@inheritdoc}
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults(array(
-            'data_class' => 'CEOFESABundle\Entity\Parcours'
-        ));
+        $resolver
+            ->setRequired(array('structure'))
+            ->setDefaults(array('data_class' => 'CEOFESABundle\Entity\Parcours'))
+            ->setAllowedTypes(array('structure' => 'int'));
     }
 
     /**
-     * @return string
+     * {@inheritdoc}
      */
     public function getName()
     {
